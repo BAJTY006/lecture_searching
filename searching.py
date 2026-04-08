d51c0b4b002a16c9ecdf4393229687ec67c63d92
Vaclav Bajtek <283148@vutbr.cz>
2026-04-08 09:25:57 +0200

    ukol 2 hotový

--- searching.py
+++ searching.py
@@ -19,12 +19,39 @@ def read_data(file_name, field):
     """
     # get current working directory path
     cwd_path = Path.cwd()
-    
+
     file_path = cwd_path / file_name
+    keys_list = ['unordered_numbers', 'ordered_numbers', 'dna_sequence']
+    key_set = set(keys_list)
+
+    if field not in key_set:
+        return None
+    else:
+        with open (file_path, mode="r", encoding="utf-8") as file_obj:
+            data = json.load(file_obj)
+            result = data[field]
+        return result
+
+
+# sequential_data = [54, 2, 18, 5, 3, 31, 20, 65, -10, 300, 17, 5, -1, 0, 0, 102, 7, 8, 9, 9, -3, -5, 0, 1, 63, 82, -36, -5]
+# wanted_number = 9
+def linear_search(sequential_data, wanted_number):
+    position = []
+    count = 0
+    for i, num in enumerate(sequential_data):
+        if num == wanted_number:
+            position.append(i)
+            count += 1
+    result = {"position" : position, "count" : count}
+    return result
 
 
 def main():
-    pass
+    sequential_data = read_data("sequential.json", "unordered_numbers")
+    wanted_number = 3
+    wanted_number_dict = linear_search(sequential_data, wanted_number)
+    print(sequential_data)
+    print(wanted_number_dict)
 
 
 if __name__ == "__main__":
